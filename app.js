37ef8526dec41a126f7b87e074d6566c7c4f424c
Jeff Finley <quickliketurtle@gmail.com>
2017-10-29 14:05:35 -0700

    Add generate button

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 let adjectives = ['funny', 'dumb', 'nosey', 'unlucky', 'happy'];
 let nouns = ['lizzard', 'zebra', 'rock', 'sky', 'leaf'];
 let verbs = ['running', 'burning', 'dying', 'falling', 'eating'];
+let adjective;
+let noun;
+let verb ;
+
+let result = document.querySelector('#result');
+let generate = document.querySelector('#generate');
+
+generate.addEventListener('click', (e) => {
+    e.preventDefault();
+    generateIdea();
+});
 
 function getRandomElement(arr) {
     return arr[(Math.floor(Math.random() * (arr.length)))].toUpperCase();
 }
 
-let adjective = getRandomElement(adjectives);
-let noun = getRandomElement(nouns);
-let verb = getRandomElement(verbs);
+function generateIdea() {
+    adjective = getRandomElement(adjectives);
+    noun = getRandomElement(nouns);
+    verb = getRandomElement(verbs);
 
-let box = document.querySelector('.box');
-box.innerHTML = `<h1>${adjective} ${noun} ${verb}</h1>`;
+    result.innerHTML = `${adjective} ${noun} ${verb}`;
+}
+
+document.onload = generateIdea();
